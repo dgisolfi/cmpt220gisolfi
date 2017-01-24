0945916e08fda57a6998bd17e4fcdb8380a8dcdc
dgisolfi <daniel.gisolfi1@marist.edu>
2017-01-24 17:03:27 -0500

    may have done wrong problems updated some

--- labs/lab1.java
+++ labs/lab1.java
@@ -77,7 +77,7 @@ public class DisplayTime {
 
 //2.5
 
-import java.util.Scanner;
+import java.util.Scannergit ;
 
 public class DisplayTime {
 	public static void main(String[] args) {
@@ -99,3 +99,32 @@ public class DisplayTime {
 
 }
 
+
+
+//2.9
+package lab1;
+import java.util.Scanner;
+ 
+ 
+public class Problem29 {
+ 
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+         
+        //Prompt user for v0, v1, and t
+        System.out.print("Enter v0, v1, and t: ");
+         
+        double v0 = input.nextDouble();
+        double v1 = input.nextDouble();
+        double t = input.nextDouble();
+         
+        //Compute average acceleration
+        double avgAcceleration = (v1 - v0) / t;
+         
+        //Display results
+        System.out.println("The average acceleration is: " + avgAcceleration);
+         
+ 
+    }
+ 
+}
