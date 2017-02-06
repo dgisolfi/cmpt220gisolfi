a81986504999c8c8ce422a05eb5fa37c813134b2
dgisolfi <daniel.gisolfi1@marist.edu>
2017-02-06 18:59:39 -0500

    completed question 5.5 and Lab3

---
+++ labs/Lab3/weightConvert.java
@@ -0,0 +1,14 @@
+//CMPT 220
+//Author: Daniel Gisolfi
+//Date: 2.6.17
+//lab 3
+
+//5.5
+
+public class weightConvert {
+    public static void main(String[] args) {
+        System.out.println("Kilograms   Pounds   |   Pounds   Kilograms");
+        for (int i = 1, j = 20; i < 200; i += 2, j += 5) {
+            System.out.printf("%-9d%9.1f   |   %-9d%9.2f%n", i, i * 2.2, j, j / 2.2);
+        }
+    }
