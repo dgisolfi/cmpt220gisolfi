d02b57da13f9d3919ce89c9b770794ce20c13ee3
dgisolfi <daniel.gisolfi1@marist.edu>
2017-03-07 14:12:59 -0500

    Finished Question 9.5

---
+++ labs/Lab6/GregorianCalendarClass.java
@@ -0,0 +1,26 @@
+//CMPT 220
+//Author: Daniel Gisolfi
+//Date: 2.5.17
+//lab 6
+
+//9.5
+
+public class GregorianCalendarClass {
+    public static void main(String[] args) {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+
+        System.out.println("Current");
+        printDate(gregorianCalendar);
+
+        System.out.println("1234567898765L");
+        gregorianCalendar.setTimeInMillis(1234567898765L);
+        printDate(gregorianCalendar);
+    }
+
+    private static void printDate(GregorianCalendar gregorianCalendar) {
+        System.out.printf("   Year:  %s%n", gregorianCalendar.get(GregorianCalendar.YEAR));
+        System.out.printf("   Month: %s%n", gregorianCalendar.get(GregorianCalendar.MONTH));
+        System.out.printf("   Day:   %s%n", gregorianCalendar.get(GregorianCalendar.DAY_OF_MONTH));
+        System.out.println();
+    }
+}
