019308bfa695c0906d0fb01da0f15be344310d33
dgisolfi <daniel.gisolfi1@marist.edu>
2017-03-28 18:28:31 -0400

    The read and write methods now work

--- prj/2/code/PassManager.java
+++ prj/2/code/PassManager.java
@@ -6,13 +6,21 @@
 import java.util.Random;
 import java.util.Scanner;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 public class PassManager {
 
 	public static void main(String[] args) {
-		menu();
+		write();
 		
 	}
 	public static void menu() {
@@ -25,46 +33,95 @@ public class PassManager {
 		System.out.println(cmd);
 
 		if (cmd == 1) {
-			create();
+			write();
 		} else if (cmd == 2) {
-			view();
+			read();
 		} else {
 			System.out.println("not a valid input");
 		}
 	}
 
-	public class Account{
-		public String accountName;
-		public String username;
-		public String password;
-	}
-	public static void create() {
+	// public class Account{
+	// 	public String accountName;
+	// 	public String username;
+	// 	public String password;
+	// }
+	public static void write() {
 		Scanner input = new Scanner(System.in);
+
 		System.out.println("Create new login:");
+
 		System.out.print("What is the name of this account? ");
 		String accountName = input.next();
+
 		System.out.print("What is the username ? ");
 		String username = input.next();
+
 		System.out.print("What is the password? ");
 		String pw = input.next();
-	}
 
-	public static void view() {
-		System.out.print("run View");
+		JSONObject accountObj = new JSONObject();
+		accountObj.put("Account Name", accountName);
+		accountObj.put("Username", username);
+		accountObj.put("Password", pw);
+ 
+		try {
+			
+			// Writing to a file
+			File file=new File("Password.json");
+			file.createNewFile();
+			FileWriter fileWriter = new FileWriter(file);
+			System.out.println("Writing JSON object to file");
+			//System.out.println("-----------------------");
+			//System.out.print(accountObj);
+ 
+			fileWriter.write(accountObj.toJSONString());
+			fileWriter.flush();
+			fileWriter.close();
+ 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+ 
+	}
+	
+	public static void read() {
+		JSONParser parser = new JSONParser();
+
+		try {
+
+			Object obj = parser.parse(new FileReader("Password.json"));
+
+			JSONObject jsonObject = (JSONObject) obj;
+		 	//Print Account Name
+			String accountName = (String) jsonObject.get("Account Name");
+			System.out.println("Name of Account: "+ accountName);
+		 	//Print UserName
+		 	String userName = (String) jsonObject.get("Username");
+			System.out.println("Username: "+ userName);
+			//Print Password
+			String password = (String) jsonObject.get("Password");
+			System.out.println("Password: "+ password);
+		 
+		  	} catch (FileNotFoundException e) {
+		   	e.printStackTrace();
+		 	} catch (IOException e) {
+		   	e.printStackTrace();
+		  	} catch (ParseException e) {
+		   	e.printStackTrace();
+		}
 	}
 
 	public static void generatePw() {
 		//Random rand = new Random();
 		//int  n = rand.nextInt(50) + 1;
 
-	    Random r = new Random();
-	    //String strongPW = " "
-	    String alphabet = "abcdefghijklmnopqrstuvwxyz123456789";
-	    for (int i = 0; i < 20; i++) {
-	  		alphabet.charAt(r.nextInt(alphabet.length()));
-	    } // prints 50 random characters from alphabet
+		Random r = new Random();
+		//String strongPW = " "
+		String alphabet = "abcdefghijklmnopqrstuvwxyz123456789";
+		for (int i = 0; i < 20; i++) {
+			alphabet.charAt(r.nextInt(alphabet.length()));
+		} // prints 50 random characters from alphabet
 	}
 
 }
-
-Youve Never meant anything to me or anyone else for more than one night
