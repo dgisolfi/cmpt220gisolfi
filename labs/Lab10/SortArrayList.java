c02c5e30ec1e0b67db6f5d7ec681412605251994
dgisolfi <daniel.gisolfi1@marist.edu>
2017-04-18 14:14:38 -0400

    Finished Question 13.3

---
+++ labs/Lab10/SortArrayList.java
@@ -0,0 +1,46 @@
+//CMPT 220
+//Author: Daniel Gisolfi
+//Date: 4.18.17
+//lab 10
+
+//13.3
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class SortArrayList {
+    public static void main(String[] args) {
+        ArrayList<Number> list = generateRandomArray(10);
+        System.out.println("Original list");
+        System.out.println(list);
+        sort(list);
+        System.out.println("After Sorting");
+        System.out.println(list);
+
+    }
+
+    private static ArrayList<Number> generateRandomArray(int size) {
+        ArrayList<Number> list = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            list.add(random.nextInt(size));
+        }
+        return list;
+    }
+
+    public static void sort(ArrayList<Number> list) {
+            boolean sorted;
+            Number temp;
+            do {
+                sorted = true;
+                for (int i = 0; i < list.size() - 1; i++) {
+                    if (list.get(i).doubleValue() > list.get(i + 1).doubleValue()) {
+                        temp = list.remove(i);
+                        list.add(i + 1, temp);
+                        sorted = false;
+                    }
+                }
+            } while (!sorted);
+
+    }
+}
